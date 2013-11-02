16bb78900afca5fe1cd81a097cf869d3a0ddd9fb
Josh Haberman <jhaberman@gmail.com>
2013-11-01 18:20:14 -0700

    Added topo sort of tasks (completely untested).

--- taskforce.cc
+++ taskforce.cc
@@ -2,25 +2,130 @@
 #include "taskforce.h"
 
 #include <errno.h>
-#include <unistd.h>
-#include <memory>
-#include <iostream>
 #include <sys/time.h>
+#include <unistd.h>
+#include <iostream>
+#include <memory>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
 #include "taskforce.pb.h"
 #include "sha1.h"
 
-using namespace taskforce;
 //using namespace std;
 using google::protobuf::RepeatedPtrField;
 
+namespace taskforce {
+
+namespace {
+
 const int kNumMicrosPerSecond = 1000000;
 
-static int64_t NowUsec() {
+int64_t NowUsec() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return (tv.tv_sec * kNumMicrosPerSecond) + tv.tv_usec;
 }
 
+template <class T, class Key>
+bool TryInsert(T* container, Key&& val,
+               typename T::iterator* iter) {
+  auto success = container->insert(std::forward<Key>(val));
+  if (success.second) {
+    *iter = success.first;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+template <class T>
+const typename T::mapped_type& FindOrDie(const T& container,
+                                         const typename T::key_type& key) {
+  typename T::const_iterator iter = container.find(key);
+  assert(iter != container.end());
+  return iter->second;
+}
+
+class TopoSort {
+ public:
+  TopoSort() {
+  }
+
+  bool Sort(const RepeatedPtrField<Task>& tasks,
+            const std::unordered_map<std::string, const Task*>* task_producing,
+            std::vector<const Task*>* sorted, std::string* error) {
+    error_ = error;
+    sorted->clear();
+    sorted_ = sorted;
+    unseen_.clear();
+    visiting_.clear();
+    stack_.clear();
+    task_producing_ = task_producing;
+
+    for (const auto& task : tasks) {
+      unseen_.insert(&task);
+    }
+
+    for (TaskSet::iterator it = unseen_.begin(); it != unseen_.end(); ) {
+      assert(stack_.size() == 0);
+      stack_.push_back(*it);
+      if (!Visit(*it)) {
+        return false;
+      }
+      stack_.pop_back();
+      unseen_.erase(it++);
+    }
+
+    return true;
+  }
+
+  bool Visit(const Task* task) {
+    TaskSet::iterator iter;
+
+    if (!TryInsert(&visiting_, task, &iter)) {
+      *error_ = "Dependency cycle: [" + task->label();
+      while (stack_.back() != task) {
+        *error_ += ", " + stack_.back()->label();
+        stack_.pop_back();
+      }
+      *error_ += "]";
+      return false;
+    }
+
+    stack_.push_back(task);
+    if (stack_[0] != task) {
+      unseen_.erase(task);
+    }
+
+    for (const auto& src : task->source()) {
+      Visit(FindOrDie(*task_producing_, src));
+    }
+
+    stack_.pop_back();
+    visiting_.erase(iter);
+    sorted_->push_back(task);
+    return true;
+  }
+
+ private:
+  typedef std::unordered_set<const Task*> TaskSet;
+  TaskSet unseen_;
+  TaskSet visiting_;
+
+  // Solely for the purposes of error reporting, so we can print the cycle.
+  std::vector<const Task*> stack_;
+
+  std::vector<const Task*>* sorted_;
+  std::string* error_;
+
+  const std::unordered_map<std::string, const Task*>* task_producing_;
+};
+
+}  // anoymous namespace
+
+// For constructing arrays of strings in the slightly peculiar format
+// required by execve().
 class StrArr {
  public:
   StrArr(const RepeatedPtrField<std::string>& field)
@@ -33,33 +138,65 @@ class StrArr {
   }
 
   ~StrArr() {
+    // unique_ptr frees the array of pointers but not the pointed-to strings.
     for (int i = 0; i < size_; i++) {
       free(array_[i]);
     }
+  }
+
+  void AppendFrom(const StrArr& other) {
+    size_t new_size = size_ + other.size_;
+    std::unique_ptr<char*[]> new_array(new char*[new_size + 1]);
+    std::copy(&array_[0], &array_[size_], &new_array[0]);
+    array_.swap(new_array);
+
+    for (int i = 0; i < other.size_; i++) {
+      array_[size_ + i] = strdup(other.array_[i]);
+    }
+    std::swap(size_, new_size);
+    array_[size_] = NULL;
   }
 
   char **get() { return array_.get(); }
 
  private:
   size_t size_;
+  // Can't use vector<char*> because execve() takes ptr to non-const array.
   std::unique_ptr<char*[]> array_;
 };
 
-bool TaskForce::Initialize(const std::vector<Task*>& tasks,
+bool TaskForce::Initialize(std::unique_ptr<TaskForceProto> taskforce,
                            std::string* error) {
-  for (const auto& task : tasks) {
-    task->clear_id();
-    std::string serialized;
-    task->SerializeToString(&serialized);
-    task->set_id(GetSHA1(serialized));
+  proto_ = std::move(taskforce);
+  base_env_.reset(new StrArr(proto_->base_env()));
 
-    Task*& entry = tasks_[task->id()];
+  // Initialize tasks_: tasks indexed by their SHA1-derived ID.
+  for (const auto& task : proto_->task()) {
+    std::string serialized;
+    task.SerializeToString(&serialized);
+    const Task*& entry = tasks_[GetSHA1(serialized)];
     if (entry) {
       tasks_.clear();
       *error = "Duplicate task: " + serialized;
     }
-    entry = task;
+    entry = &task;
+
+    // Initialize task_producing_.
+    //
+    // TODO: normalize the pathnames somehow?
+    for (const auto& target : task.target()) {
+      task_producing_[target] = &task;
+    }
   }
+
+  // Initialize topo_order_: a topographical order of Tasks that respects their
+  // declared dependencies.
+  //
+  // There are various things we could do to be more clever about the ordering.
+  if (!TopoSort().Sort(proto_->task(), &task_producing_, &topo_order_, error)) {
+    return false;
+  }
+
   return true;
 }
 
@@ -75,11 +212,12 @@ bool TaskForce::SpawnJob(Job* job) {
     return true;
   } else {
     // Child.  Setup argv and environment.
-    Task* task = tasks_[job->task_id()];
+    const Task* task = tasks_[job->task_id()];
     assert(task);
 
     StrArr arg(task->arg());
     StrArr env(task->env());
+    env.AppendFrom(*base_env_.get());
     execve(task->arg(0).c_str(), arg.get(), env.get());
 
     // If we got here it's failure.
@@ -91,3 +229,5 @@ bool TaskForce::SpawnJob(Job* job) {
 bool TaskForce::Build() {
   return true;
 }
+
+}  // namespace taskforce
